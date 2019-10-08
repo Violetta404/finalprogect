1cb5fca29fb9a1c4f77c974cebbf97411b47cc56
Violetta <smavi7777@gmail.com>
2019-10-08 16:05:43 +0300

    third commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,8 +1,41 @@
-$('.slider-team').slick({
+$('.slider-carousel').slick({
+  dots: true,
   infinite: true,
-  slidesToShow: 4,
-  slidesToScroll: 1
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
 });
 
-$('.carousel').carousel();
 
+$('.slider-team').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
